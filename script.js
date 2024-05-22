9dddffaf6f645fc67702ee0200a060853ce8b27f
ilhamfajaarr <157339660+ilhamfajaarr@users.noreply.github.com>
2024-05-22 09:54:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const display = document.getElementById("display");
+let timer = null;
+let starttime = 0;
+let elapsedtime = 0;
+let isRunning = false;
+
+function start(){
+    if(!isRunning){
+        starttime = Date.now() - elapsedtime;
+        timer = setInterval(update,10);
+        isRunning = true;
+    }
+
+}
+
+function stop(){
+    if(isRunning){
+        clearInterval(timer);
+        elapsedtime = Date.now() - starttime;
+        isRunning = false;
+    }
+
+}
+
+function reset(){
+    clearInterval(timer);
+    starttime = 0;
+    elapsedtime = 0;
+    isRunning = false;
+    display.textContent = "00:00:00:00";    
+    
+
+}
+
+function update(){
+    const CurrentTime = Date.now();
+    elapsedtime = CurrentTime - starttime;
+
+    let hours = Math.floor(elapsedtime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedtime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedtime / 1000 % 60);
+    let miliseconds = Math.floor(elapsedtime % 1000 / 10);
+
+    hours = String(hours).padStart(2,"0");
+    minutes = String(minutes).padStart(2,"0");
+    seconds = String(seconds).padStart(2,"0");
+    miliseconds = String(miliseconds).padStart(2,"0");
+
+    display.textContent = `${hours}:${minutes}:${seconds}:${miliseconds}`
+
+}
